f5c2b622132f69b971275d57417f9742f2bc8a97
azimbabu <azim.babu@gmail.com>
2022-12-15 21:47:03 -0800

    remove secrets

--- ch07/grpc-opentelemetry/product-info-server/src/main/java/ecommerce/OpenTelemetryConfiguration.java
+++ ch07/grpc-opentelemetry/product-info-server/src/main/java/ecommerce/OpenTelemetryConfiguration.java
@@ -21,66 +21,69 @@ import java.util.concurrent.TimeUnit;
 
 public class OpenTelemetryConfiguration {
 
-    private static final int DEFAULT_PROMETHEUS_PORT = 19090;
-    private static final String PROMETHEUS_PORT_PROPERTY = "prometheus.port";
+  private static final int DEFAULT_PROMETHEUS_PORT = 19090;
+  private static final String PROMETHEUS_PORT_PROPERTY = "prometheus.port";
 
-    private static final String JAEGAR_ENDPOINT = "http://localhost:4317";
+  private static final String JAEGAR_ENDPOINT = "http://localhost:4317";
 
-    public static OpenTelemetry initOpenTelemetry() {
-        Resource resource =
-                Resource.getDefault()
-                        .merge(
-                                Resource.create(
-                                        Attributes.of(ResourceAttributes.SERVICE_NAME, "product-info-server")));
+  public static OpenTelemetry initOpenTelemetry() {
+    Resource resource =
+        Resource.getDefault()
+            .merge(
+                Resource.create(
+                    Attributes.of(ResourceAttributes.SERVICE_NAME, "product-info-server")));
 
-        LoggingSpanExporter loggingSpanExporter = LoggingSpanExporter.create();
-        // Export traces to Jaeger over OTLP
-        OtlpGrpcSpanExporter jaegarOtlpExporter = OtlpGrpcSpanExporter.builder()
-                .setEndpoint(JAEGAR_ENDPOINT)
-                .setTimeout(30, TimeUnit.SECONDS)
-                .build();
-        LoggingMetricExporter metricExporter = LoggingMetricExporter.create();
-        //      OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder().build();
-        //      OtlpGrpcMetricExporter metricExporter = OtlpGrpcMetricExporter.builder().build();
-        MetricReader prometheusReader = PrometheusHttpServer.builder().setPort(getPrometheusPort()).build();
+    LoggingSpanExporter loggingSpanExporter = LoggingSpanExporter.create();
+    // Export traces to Jaeger over OTLP
+    OtlpGrpcSpanExporter jaegarOtlpExporter =
+        OtlpGrpcSpanExporter.builder()
+            .setEndpoint(JAEGAR_ENDPOINT)
+            .setTimeout(30, TimeUnit.SECONDS)
+            .build();
+    LoggingMetricExporter metricExporter = LoggingMetricExporter.create();
+    //      OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder().build();
+    //      OtlpGrpcMetricExporter metricExporter = OtlpGrpcMetricExporter.builder().build();
+    MetricReader prometheusReader =
+        PrometheusHttpServer.builder().setPort(getPrometheusPort()).build();
 
-        SdkTracerProvider sdkTracerProvider =
-                SdkTracerProvider.builder()
-                        .addSpanProcessor(BatchSpanProcessor.builder(loggingSpanExporter).build())
-                        .addSpanProcessor(BatchSpanProcessor.builder(jaegarOtlpExporter).build())
-                        .setResource(resource)
-                        .build();
+    SdkTracerProvider sdkTracerProvider =
+        SdkTracerProvider.builder()
+            .addSpanProcessor(BatchSpanProcessor.builder(loggingSpanExporter).build())
+            .addSpanProcessor(BatchSpanProcessor.builder(jaegarOtlpExporter).build())
+            .setResource(resource)
+            .build();
 
-        SdkMeterProvider sdkMeterProvider =
-                SdkMeterProvider.builder()
-                        .registerMetricReader(PeriodicMetricReader.builder(metricExporter).build())
-                        .registerMetricReader(prometheusReader)
-                        .setResource(resource)
-                        .build();
+    SdkMeterProvider sdkMeterProvider =
+        SdkMeterProvider.builder()
+            .registerMetricReader(PeriodicMetricReader.builder(metricExporter).build())
+            .registerMetricReader(prometheusReader)
+            .setResource(resource)
+            .build();
 
-        OpenTelemetrySdk openTelemetrySdk =
-                OpenTelemetrySdk.builder()
-                        .setTracerProvider(sdkTracerProvider)
-                        .setMeterProvider(sdkMeterProvider)
-                        // install the W3C Trace Context propagator
-                        .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-                        .buildAndRegisterGlobal();
+    OpenTelemetrySdk openTelemetrySdk =
+        OpenTelemetrySdk.builder()
+            .setTracerProvider(sdkTracerProvider)
+            .setMeterProvider(sdkMeterProvider)
+            // install the W3C Trace Context propagator
+            .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+            .buildAndRegisterGlobal();
 
-        Runtime.getRuntime()
-                .addShutdownHook(
-                        new Thread(
-                                () -> {
-                                    System.err.println(
-                                            "*** Forcing the Trace Provider and Meter Provider to shutdown and process the remaining spans");
-                                    sdkTracerProvider.shutdown();
-                                    sdkMeterProvider.shutdown();
-                                    System.err.println("*** TTrace Provider and Meter Provider shut down");
-                                }));
-        return openTelemetrySdk;
-    }
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread(
+                () -> {
+                  System.err.println(
+                      "*** Forcing the Trace Provider and Meter Provider to shutdown and process the remaining spans");
+                  sdkTracerProvider.shutdown();
+                  sdkMeterProvider.shutdown();
+                  System.err.println("*** TTrace Provider and Meter Provider shut down");
+                }));
+    return openTelemetrySdk;
+  }
 
-    private static int getPrometheusPort() {
-        return System.getProperties().containsKey(PROMETHEUS_PORT_PROPERTY) ?
-                Integer.parseInt(System.getProperty(PROMETHEUS_PORT_PROPERTY)) : DEFAULT_PROMETHEUS_PORT;
-    }
+  private static int getPrometheusPort() {
+    return System.getProperties().containsKey(PROMETHEUS_PORT_PROPERTY)
+        ? Integer.parseInt(System.getProperty(PROMETHEUS_PORT_PROPERTY))
+        : DEFAULT_PROMETHEUS_PORT;
+  }
 }
